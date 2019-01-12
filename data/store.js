bb0a0c15947578e54e3130c3ece7ea6a984d3377
fanxinzhi-FXZ <2313927666@qq.com>
2019-01-12 17:33:14 +0800

    MOD: 增加轮播图

---
+++ data/store.js
@@ -0,0 +1,13 @@
+module.exports = {
+  index: {
+    // 首页banner图配置
+    bannerItems: [
+      {
+        "bannerImg": "http://www.beishuibao.com/web_pic/program/banner.png",
+      },
+      {
+        "bannerImg": "http://www.beishuibao.com/web_pic/program/banner2.png",
+      }
+    ],
+  }
+}
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,30 @@
 //index.js
-const app = getApp()
+const store = require('../../data/store.js');
 
 Page({
   data: {
+    bannerImgItems: store.index.bannerItems,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 500,
+    circular: true,
+    indicatorActiveColor: "#ffffff",
 
+    swiperHeight: 0,
+    swiperWidth: 0,
   },
 
   onLoad: function() {
-
+    var vm = this;
+    // 获取屏幕宽度，设置banner的宽高
+    wx.getSystemInfo({
+      success: function (res) {
+        vm.setData({
+          swiperHeight: res.screenWidth / 2.4193,
+          swiperWidth: res.screenWidth
+        })
+      }
+    })
   }
 })
