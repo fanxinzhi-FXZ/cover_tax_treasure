bfd6ba35c8e0bac93562b14d4decf02e87ae1217
fanxinzhi-FXZ <2313927666@qq.com>
2019-01-12 23:11:38 +0800

    MOD: 首页样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,133 @@ Page({
   data: {
     bannerImgItems: store.index.bannerItems,
     indicatorDots: true,
-    autoplay: true,
+    swiperautoplay: true,
     interval: 5000,
     duration: 500,
     circular: true,
+    productCircular: false,
     indicatorActiveColor: "#ffffff",
+    productActiveColor: "#4d6ce3",
 
     swiperHeight: 0,
     swiperWidth: 0,
+
+    productItems:[
+      {
+        "icon": "http://www.beishuibao.com/web_pic/program/list1_icon_gold.png",
+        "title": "黄金计划",
+        "data":  [
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+        ]
+      },
+      {
+        "icon": "http://www.beishuibao.com/web_pic/program/list1_icon_diamond.png",
+        "title": "钻石计划",
+        "data": [
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+        ]
+      },
+      {
+        "icon": "http://www.beishuibao.com/web_pic/program/list1_icon_crown.png",
+        "title": "大富翁计划",
+        "data": [
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+          {
+            "title": "理赔额度",
+            "text": "500"
+          },
+        ]
+      }
+    ]
   },
 
   onLoad: function() {
