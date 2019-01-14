9951e425b90f6342405125673752dfed7adfb2cc
yangkuanli <yangkuanli@tongbill.com>
2019-01-15 00:25:06 +0800

    fix

---
+++ pages/modifyInfo/modifyInfo.js
@@ -0,0 +1,15 @@
+// pages/modifyInfo/modifyInfo.js
+Page({
+  data: {
+    unselect:'http://www.beishuibao.com/web_pic/program/icon_unselect.png',
+    select: 'http://www.beishuibao.com/web_pic/program/icon_select.png'
+  },
+  // 姓名输入框获取
+  fullNameFunc:function () {
+
+  },
+  // 手机号输入框获取
+  mobileFunc:function (){
+
+  }
+})
